82d655786a8e2d2fa98122419f4f8eba2d26ae55
Valentin BEAURY <vbeaury@sqli.com>
2021-10-15 11:53:25 +0200

    first commit

---
+++ RevisionVCE/wwwroot/js/Question.js
@@ -0,0 +1,18 @@
+﻿class Question {
+    constructor(questionData) {
+        this.questionData = questionData;
+        this.answer = questionData.answer.trim();
+        this.isQCM = this.answer.trim().length > 1;
+        this.answers = this.answer.split('');
+    }
+
+    isCorrect(answerArray) {
+        if (arrayEquals(this.answers, answerArray)) {
+            $("#score").text(parseInt($("#score").text()) + 1)
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
--- RevisionVCE/wwwroot/js/site.js
+++ RevisionVCE/wwwroot/js/site.js
@@ -1,69 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
 
-// Write your JavaScript code.
-$(document).ready(function () {
-    function displayAnswer(id) {
-        $("#" + id).show();
-    }
+function shuffleArray(inputArray) {
+    inputArray.sort(() => Math.random() - 0.5);
+}
 
-    function vrai(id) {
-        $("#" + id).css('color', 'green');
-        $("#current").text(parseInt($("#current").text()) + 1);
-    }
-
-    function faux(id) {
-        $("#" + id).css('color', 'red');
-    }
-
-    $.getJSON("data.json", function (data) {
-        for (var i = 0; i < data.length; i++) {
-            let questionContent = $("<div class='mb-5' id='text-" + data[i].questionNumber  + "'></div>");
+let quizz;
 
 
-            let questionTitle = $("<h3>QUESTION " + data[i].questionNumber + "</h3>");
-            let questionText = $("<p>" + data[i].questionTxt + "</p>");
-            let questionNote = $("<p style='font-weight:bold'>" + data[i].questionNote + "</p>")
-            let choicesListe = $("<ul></ul>");
+$(async () => {
+    quizz = await new Quizz(
+        $("#valider")
+        , $("#questions-container")
+        , $("#explanation")
+        , $("#section")
+        , $("#questionTxt")
+        , $("#validation-container")
+        , $("#next-container")
+        , $("#next"));
 
-            if (data[i].questionPossibilities != undefined) {
-                for (var j = 0; j < data[i].questionPossibilities.length; j++) {
-                    choicesListe.append($("<li style='font-weight:350'>" + data[i].questionPossibilities[j] + "</li>"))
-                }
-            }
+    quizz.questionIndex = 0;
+    quizz.setQuestion();
 
-            questionContent.append(questionTitle);
-            questionContent.append(questionText);
-            questionContent.append(questionNote);
-            questionContent.append(choicesListe);
+    $("#valider").click(function () {
+        quizz.showExplanation(true);
+    })
 
-            
-            let hiddenDiv = $("<div style='display:none' id='" + data[i].questionNumber + "'></div>");
-            hiddenDiv.append($("<p ><strong>Réponse : " + data[i].answer + "</strong><br/>" + "<strong>Explanation :</strong> <br/>" + data[i].explanation + "</p>"))
-            hiddenDiv.append($("<div class='row ml-1'><button data-id-cible='text-" + data[i].questionNumber + "' class='btn btn-success mr-3 vrai'>J'ai eu bon</button><button data-id-cible='text-" + data[i].questionNumber + "' class='btn btn-danger faux'>J'ai eu faux</button></div>"));
-            questionContent.append($("<button data-id-cible='" + data[i].questionNumber + "' class='btn btn-primary show-answer'>Voir la réponse</button>"));
-            questionContent.append(hiddenDiv);
-            
-            $("#questions").append(questionContent);
-            $("#max").text(data.length)
-        }
-    }).fail(function () {
-        console.log("An error has occurred.");
-    }).then(() => {
-        $(".show-answer").click(function () {
-            displayAnswer($(this).attr("data-id-cible"));
-            $(this).hide();
-        });
-        $(".vrai").click(function () {
-            vrai($(this).attr("data-id-cible"));
-            $(this).hide();
-            $(".faux:visible").hide();
-        })
-
-        $(".faux").click(function () {
-            faux($(this).attr("data-id-cible"));
-            $(this).hide();
-            $(".vrai:visible").hide();
-        })
-    });
+    $("#next").click(function () {
+        quizz.nextQuestion();
+    })
 });
